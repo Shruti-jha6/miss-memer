aa27c6ab375bab113a6cec217761982175179b76
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-08-11 23:03:50 +0530

    last change done

--- script.js
+++ script.js
@@ -25,6 +25,21 @@ const generateMemeBtn = document.querySelector(
         console.error("There was a problem with the fetch operation:", error);
       });
   };
+
+  // const generateMeme = () => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const memes = data.data.memes;
+  //       const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+  //       updateDetails(randomMeme.url, randomMeme.name, "Imgflip");
+  //     })
+  //     .catch((error) => {
+  //       memeTitle.innerHTML = "Failed to load meme. Try again!";
+  //       memeAuthor.innerHTML = "";
+  //       console.error("There was a problem with the fetch operation:", error);
+  //     });
+  // };
   
   
   
